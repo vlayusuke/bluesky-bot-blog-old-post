548319a05d59b3be10321ad50197e87aee14f60d
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-04-28 19:05:26 +0900

    add: 共通Local変数の実装

---
+++ terraform/base_locals.tf
@@ -0,0 +1,15 @@
+# ================================================================================
+# Base Local Values
+# ================================================================================
+locals {
+  # repository info
+  repository = "vlayusuke/bluesky-bot-blog-old-post"
+
+  # project info
+  project = "bluesky-bot"
+  author  = "Yusuke TOMIOKA"
+  email   = "vlayusuke@gmail.com"
+
+  # domain
+  domain = "vlayusuke.net"
+}
